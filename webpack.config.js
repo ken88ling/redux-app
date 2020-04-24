d5c7fadf4366eb25acc30af55454073ab7b7805d
ken88ling <ken88ling@gmail.com>
2020-04-25 00:19:53 +0800

    setup env

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,8 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
-    contentBase: path.joinj(__dirname, 'dist'),
-    port: 9000,
+    contentBase: path.join(__dirname, 'dist'),
+    port: 9002,
   },
   mode: 'development',
 };
