4685eb8502ebc9d3f5f260e945c08641c82085d6
ken88ling <ken88ling@gmail.com>
2020-04-25 17:32:04 +0800

    const action

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import store from './store';
+import * as actions from './actionTypes';
 
 const unsubscribe = store.subscribe(() => {
   console.log('store changed', store.getState());
 });
 
 store.dispatch({
-  type: 'bugAdded',
+  type: actions.BUG_ADDED,
   payload: {
     description: 'Bug1',
   },
@@ -13,7 +14,7 @@ store.dispatch({
 
 unsubscribe();
 store.dispatch({
-  type: 'bugRemoved',
+  type: actions.BUG_REMOVED,
   payload: {
     id: 1,
   },
