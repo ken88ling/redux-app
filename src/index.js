ef75c9905aa905df52d555235e7761e5059f3be9
ken88ling <ken88ling@gmail.com>
2020-04-26 01:55:22 +0800

    duck pattern

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import store from './store';
-import { bugAdded, bugRemoved, bugResolved } from './actions';
+import configureStore from './store/configureStore';
+import * as actions from './store/bugs';
 
+const store = configureStore();
 const unsubscribe = store.subscribe(() => {
   console.log('store changed', store.getState());
 });
 
-store.dispatch(bugAdded('bug 1'));
-store.dispatch(bugAdded('bug 2'));
-store.dispatch(bugAdded('bug 3'));
-store.dispatch(bugResolved(1));
+store.dispatch(actions.bugAdded('bug 1'));
+store.dispatch(actions.bugAdded('bug 2'));
+store.dispatch(actions.bugAdded('bug 3'));
+store.dispatch(actions.bugResolved(1));
 
 unsubscribe();
-// store.dispatch(bugRemoved(1));
+store.dispatch(actions.bugRemoved(2));
+
 console.log('state => ', store.getState());
