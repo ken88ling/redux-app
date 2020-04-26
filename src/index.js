a145ed496c29ddaf44ac7ca936631438c73c64f1
ken88ling <ken88ling@gmail.com>
2020-04-26 10:49:16 +0800

    using toolkit - create actions

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import configureStore from './store/configureStore';
-import * as actions from './store/bugs';
+import configureStore from "./store/configureStore";
+import * as actions from "./store/bugs";
 
 const store = configureStore();
 const unsubscribe = store.subscribe(() => {
-  console.log('store changed', store.getState());
+  console.log("store changed", store.getState());
 });
 
-store.dispatch(actions.bugAdded('bug 1'));
-store.dispatch(actions.bugAdded('bug 2'));
-store.dispatch(actions.bugAdded('bug 3'));
-store.dispatch(actions.bugResolved(1));
+store.dispatch(actions.bugAdded({ description: "bug 1" }));
+store.dispatch(actions.bugAdded({ description: "bug 2" }));
+store.dispatch(actions.bugAdded({ description: "bug 3" }));
+store.dispatch(actions.bugResolved({ id: 1 }));
 
 unsubscribe();
-store.dispatch(actions.bugRemoved(2));
 
-console.log('state => ', store.getState());
+store.dispatch(actions.bugRemoved({ id: 2 }));
+
+console.log("state => ", store.getState());
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,63 +1,32 @@
-const BUG_ADDED = 'bugAdded';
-const BUG_REMOVED = 'bugRemoved';
-const BUG_RESOLVED = 'bugResolved';
+import { createAction } from '@reduxjs/toolkit';
 
-
-export const bugAdded = (description) => ({
-    type: BUG_ADDED,
-    payload: {
-        description,
-    },
-});
-// export function bugAdded(description) {
-//   return {
-//     type: BUG_ADDED,
-//     payload: {
-//       description: 'Bug1',
-//     },
-//   };
-// }
-
-export function bugRemoved(id) {
-    return {
-        type: BUG_REMOVED,
-        payload: {
-            id,
-        },
-    };
-}
-
-export const bugResolved = (id) => ({
-    type: BUG_RESOLVED,
-    payload: {
-        id,
-    },
-});
-
+export const bugAdded = createAction('bugAdded');
+export const bugRemoved = createAction('bugRemoved');
+export const bugResolved = createAction('bugResolved');
 
 let lastId = 0;
 
 export default function reducer(state = [], action) {
-    switch (action.type) {
-        case BUG_ADDED:
-            return [
-                ...state,
-                {
-                    id: ++lastId,
-                    description: action.payload.description,
-                    resolved: false,
-                },
-            ];
+  switch (action.type) {
+    case bugAdded.type:
+      return [
+        ...state,
+        {
+          id: ++lastId,
+          description: action.payload.description,
+          resolved: false,
+        },
+      ];
 
-        case BUG_RESOLVED:
-            return state.map((bug) =>
-                bug.id !== action.payload.id ? bug : {...bug, resolved: true}
-            );
+    case bugResolved.type:
+      return state.map((bug) =>
+        bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
+      );
 
-        case BUG_REMOVED:
-            return state.filter((bug) => bug.id !== action.payload.id);
+    case bugRemoved.type:
+      return state.filter((bug) => bug.id !== action.payload.id);
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 }
