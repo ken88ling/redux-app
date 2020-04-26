3ce5ef80301a86ce1b6c8b4df3722f39a1395bd9
ken88ling <ken88ling@gmail.com>
2020-04-26 12:26:18 +0800

    design store with multiple entities

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import configureStore from './store/configureStore';
 import * as actions from './store/bugs';
+import { projectAdded } from './store/projects';
 
 const store = configureStore();
 const unsubscribe = store.subscribe(() => {
   console.log('store changed', store.getState());
 });
 
+store.dispatch(projectAdded({ name: 'project 1' }));
 store.dispatch(actions.bugAdded({ description: 'bug 1' }));
 store.dispatch(actions.bugAdded({ description: 'bug 2' }));
+
+unsubscribe();
 store.dispatch(actions.bugAdded({ description: 'bug 3' }));
 store.dispatch(actions.bugResolved({ id: 1 }));
 
-unsubscribe();
-
-console.log('state => ', store.getState());
+// console.log('state => ', store.getState());
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,6 @@
-import {configureStore} from "@reduxjs/toolkit";
-import reducer from './bugs';
-
+import { configureStore } from '@reduxjs/toolkit';
+import reducer from './reducer';
 
 export default function () {
-    return configureStore({reducer});
+  return configureStore({ reducer });
 }
-
-
