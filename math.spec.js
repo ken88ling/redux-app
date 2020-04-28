1c249b5a11b708e1ddf328f4753ca94759267425
ken88ling <ken88ling@gmail.com>
2020-04-28 12:46:21 +0800

    basic unit test

--- math.spec.js
+++ math.spec.js
@@ -1,3 +1,13 @@
-it('first test', () => {
-  console.log('test');
+import { isEven } from './src/math';
+
+describe('isEven', () => {
+  it('should return true if you give even number', () => {
+    // Function under test (SUT)
+    const result = isEven(2);
+    expect(result).toEqual(true);
+  });
+  it('should return false if you give odd number', () => {
+    const result = isEven(3);
+    expect(result).toEqual(false);
+  });
 });
