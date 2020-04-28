732a1feb948d66eb1d54b6b63a27c6ad06d4b083
ken88ling <ken88ling@gmail.com>
2020-04-29 00:44:04 +0800

    axios-mock-adapter

--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 unit test components
 yarn add jest @types/jest @babel/core @babel/preset-env babel-jest --dev
 yarn add @babel/plugin-transform-runtime --dev (for testing only)
+yarn add --dev axios-mock-adapter
--- src/store/tests/bugs.spec.js
+++ src/store/tests/bugs.spec.js
@@ -1,12 +1,18 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 import { addBug } from '../bugs';
 import configureStore from '../configureStore';
 
 describe('bugsSlice', () => {
   it('should handle the addBug action', async () => {
-    const store = configureStore();
     const bug = { description: 'a' };
+    const saveBug = { ...bug, id: 1 };
+    const fakeAxios = new MockAdapter(axios);
+    fakeAxios.onPost('/bugs').reply(200, saveBug);
+
+    const store = configureStore();
     await store.dispatch(addBug(bug));
-    expect(store.getState().entities.bugs.list).toHaveLength(1);
+    expect(store.getState().entities.bugs.list).toContainEqual(saveBug);
     // console.log(store.getState());
   });
 });
